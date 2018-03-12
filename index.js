e4f6d77555c77d698ccba1dfa60de858f8ba2ee5
KunHsuan <h17561122@hotmail.com>
2018-03-12 10:09:51 +0800

    Modify index.js

--- index.js
+++ index.js
@@ -1,78 +1,14 @@
-/*
-const http = require('http');
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-
+var http = require('http'); 
+ 
+var server = http.createServer(function (req, res) {   
+ 
+    if (req.url == '/data') { //check the URL of the current request
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ message: "Hello World"}));
+            res.end();
+    }
 });
-server.listen(PORT);
-*/
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import request from 'request';
-//import config from '../config';
-
-const app = express();
-const port = '7123';
-const VERIFY_TOKEN = 'YOUR_TOKEN';
-const PAGE_TOKEN = 'PAGE_TOKEN';
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-})
-
-app.post('/webhook/', (req, res) => {
-
-  console.log(req.body);
-
-  const messaging_events = req.body.entry[0].messaging;
-  for (let i = 0; i < messaging_events.length; i++) {
-    const event = req.body.entry[0].messaging[i];
-    const sender = event.sender.id;
-    if (event.message && event.message.text) {
-      const text = event.message.text;
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-    }
-  }
-  res.sendStatus(200);
-});
-
-app.listen(port, () => console.log(`listening on port ${port}`));
-
-function sendTextMessage(sender, text) {
-  
-  const messageData = {
-    text: text
-  }
-
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {
-        access_token:PAGE_TOKEN
-    },
-    method: 'POST',
-    json: {
-      recipient: {
-        id: sender
-      },
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
+ 
+server.listen(5000);
+ 
+console.log('Node.js web server at port 5000 is running..')
